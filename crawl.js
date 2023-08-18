33c560558cab0c63286efd34f91a52a19e541dd8
brianMac <brianmaclopez@gmail.com>
2023-08-18 12:26:01 -0700

    normalizeURL and tests

---
+++ crawl.js
@@ -0,0 +1,12 @@
+function normalizeURL(URLString) {
+  const urlObj = new URL(URLString)
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+}
+
+module.exports = {
+  normalizeURL
+}

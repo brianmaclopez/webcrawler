84ec57c012102f5c29fd2cea2d0d966cf30cfcf6
brianMac <brianmaclopez@gmail.com>
2023-08-18 13:32:08 -0700

    getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,29 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseUrl) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll('a');
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === '/') {
+      try {
+        const urlObj = new URL(`${baseUrl}${linkElement.href}`)
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with relative URL: ${err.message}`);
+      }
+    } else {
+      try {
+        const urlObj = new URL(linkElement.href)
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with absolute URL: ${err.message}`);
+      }     
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(URLString) {
   const urlObj = new URL(URLString)
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +34,6 @@ function normalizeURL(URLString) {
 }
 
 module.exports = {
-  normalizeURL
+  normalizeURL, 
+  getURLsFromHTML
 }
